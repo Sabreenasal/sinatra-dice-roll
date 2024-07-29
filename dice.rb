5ca0c9373597dedd3a2ab3acab30994590ee96bb
Sabreena Salomon <salomonbree@gmail.com>
2024-07-29 18:11:25 +0000

    Added new paths

--- dice.rb
+++ dice.rb
@@ -5,6 +5,39 @@ get("/zebra") do
   "We must add a route for each path we want to support"
 end
 
+
 get("/giraffe") do
   "Hopefully this shows up without having to restart the server 🤞🏾"
+end
+
+get("/dice/2/6") do
+  first_die = rand(1..6)
+  second_die = rand(1..6)
+  sum = first_die + second_die
+	
+  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+	
+  "<h1>2d6</h1>
+   <p>#{outcome}</p>"
+end
+
+
+
+get("/dice/2/10") do 
+  first_die = rand(1..10)
+  second_die = rand(1..10)
+  sum = first_die = second_die
+
+  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+
+  "<h2>2d10</h2>
+  <p>#{outcome}</p>"
+end
+
+get("/dice/1/20") do 
+  first_side = rand(1..20)
+  
+  outcome = "You rolled a #{first_side}."
+  "<h3>1d20</h3>
+  <p>#{outcome}</p>"
 end
