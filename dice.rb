00d29f24286b3abb3426b0587d6585f99e9d5dc4
Sabreena Salomon <salomonbree@gmail.com>
2024-07-29 22:56:27 +0000

    added new file to views

--- dice.rb
+++ dice.rb
@@ -1,5 +1,12 @@
 require "sinatra"
 require "sinatra/reloader"
+require "better_errors"
+require "binding_of_caller"
+
+# Need this configuration for better_errors
+use(BetterErrors::Middleware)
+BetterErrors.application_root = __dir__
+BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
 get("/zebra") do
   "We must add a route for each path we want to support"
@@ -10,15 +17,17 @@ get("/giraffe") do
 end
 
 get("/") do
-  "
-  <h1>Dice Roll</h1>
-  <ul>
-    <li><a href=\"/dice/2/6\">Roll two 6-sided dice</a></li>
-    <li><a href=\"/dice/2/10\">Roll two 10-sided dice</a></li>
-    <li><a href=\"/dice/1/20\">Roll one 20-sided die</a></li>
-    <li><a href=\"/dice/5/4\">Roll five 4-sided dice</a></li>
-  </ul>
-  "
+  erb(:elephant)
+end
+
+get("/dice/2/6") do
+  first_die = rand(1..6)
+  second_die = rand(1..6)
+  sum = first_die + second_die
+
+  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+
+   erb(:two_six)
 end
 
 get("/dice/2/6") do
