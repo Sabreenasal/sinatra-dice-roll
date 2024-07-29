1efd7e87b7395902f77b734a0180183b55d4624d
Sabreena Salomon <salomonbree@gmail.com>
2024-07-29 19:49:47 +0000

    added home page

--- dice.rb
+++ dice.rb
@@ -5,9 +5,20 @@ get("/zebra") do
   "We must add a route for each path we want to support"
 end
 
-
 get("/giraffe") do
   "Hopefully this shows up without having to restart the server 🤞🏾"
+end
+
+get("/") do
+  "
+  <h1>Dice Roll</h1>
+  <ul>
+    <li><a href=\"/dice/2/6\">Roll two 6-sided dice</a></li>
+    <li><a href=\"/dice/2/10\">Roll two 10-sided dice</a></li>
+    <li><a href=\"/dice/1/20\">Roll one 20-sided die</a></li>
+    <li><a href=\"/dice/5/4\">Roll five 4-sided dice</a></li>
+  </ul>
+  "
 end
 
 get("/dice/2/6") do
@@ -17,18 +28,18 @@ get("/dice/2/6") do
 	
   outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
 	
-  "<h1>Dice Roll</h1>
+  "<h1>2d6</h1>
    <p>#{outcome}</p>"
 end
 
 get("/dice/2/10") do 
   first_die = rand(1..10)
   second_die = rand(1..10)
-  sum = first_die = second_die
+  sum = first_die + second_die
 
   outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
 
-  "<h2>2d10</h2>
+  "<h1>2d10</h1>
   <p>#{outcome}</p>"
 end
 
@@ -36,6 +47,19 @@ get("/dice/1/20") do
   first_side = rand(1..20)
   
   outcome = "You rolled a #{first_side}."
-  "<h3>1d20</h3>
+  "<h1>1d20</h1>
   <p>#{outcome}</p>"
 end
+
+get("/dice/5/4") do 
+  first_die = rand(1..4)
+  second_die = rand(1..4)
+  third_die = rand(1..4)
+  fourth_die = rand(1..4)
+  fifth_die = rand(1..4)
+  sum = first_die + second_die + third_die + fourth_die + fifth_die
+
+  outcome = "You rolled a #{first_die}, #{second_die}, #{third_die}, #{fourth_die}, and #{fifth_die} for a sum of #{sum}."
+  "<h1>5d4</h1>
+  <p>#{outcome}</p>"
+end 
